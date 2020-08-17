9d190f63892ca575cd195660cb96fa2fbd69ab7c
Rawlston Morton <68116799+ricevoy@users.noreply.github.com>
2020-08-17 14:48:54 -0400

    Delete ProjectStep5.sql

--- ProjectStep5.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [PlayerID]
-      ,[FirstName]
-      ,[LastName]
-      ,[Position]
-      ,[Active]
-      ,[PlayerNum]
-  FROM [istaproject].[dbo].[Players]
